13401510ab921715ac89bcd9db5573baf3b723e6
Akshay Kapase <akshay.b.kapase@gmail.com>
2025-02-13 11:54:26 +0530

    Update HMS

--- Hospital_Management_System/src/main/java/com/hospital/management/HospitalManagementSystem.java
+++ Hospital_Management_System/src/main/java/com/hospital/management/HospitalManagementSystem.java
@@ -10,11 +10,14 @@ import java.util.Scanner;
 
 public class HospitalManagementSystem {
 
-	private static final String url = ""; 
+//	jdbc:mysql://localhost:3306/hospital
+	private static final String url = "jdbc:mysql://localhost:3306/hospital"; 
 	
-	private static final String username = ""; 
+//	root
+	private static final String username = "root"; 
 	
-	private static final String password = ""; 
+//	Akshay@132231
+	private static final String password = "Akshay@132231"; 
 	
 	private static boolean showMenu = true;
 	
